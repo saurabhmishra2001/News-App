92f3ad05029a9400a7bbabcca43a2515d1555053
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-08-05 16:37:38 +0530

    add navbar component

---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>This is a news item 2</div>
+    )
+  }
+}
+
+export default NewsItem
