ad38bc4dc65812274d29e07232ee8b1b3114c4ff
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-08-07 16:44:06 +0530

    adding some features

--- src/components/News.js
+++ src/components/News.js
@@ -4,10 +4,32 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
+  
       <div>
-        This is a news component
-        <NewsItem />
-      </div>
+          <div className='container my-3 '>
+              <h2>Picture News -Top Headlines</h2>
+              <div className='row'>
+                    <div className='col md-4'>
+                        <NewsItem  title="My title" description ="My description"/>
+                    </div>
+                    <div className='col md-4'>
+                        <NewsItem  title="My title" description ="My description"/>
+                    </div>
+                    <div className='col md-4'>
+                        <NewsItem  title="My title" description ="My description"/>
+                    </div>
+                    <div className='col md-4'>
+                        <NewsItem  title="My title" description ="My description"/>
+                    </div>
+                    <div className='col md-4'>
+                        <NewsItem  title="My title" description ="My description"/>
+                    </div>
+                    <div className='col md-4'>
+                        <NewsItem  title="My title" description ="My description"/>
+                    </div>
+              </div>    
+        </div>
+        </div>
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,44 +1,21 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Component } from 'react';
+import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title , description}=this.props;
     return (
       <>
-      <div>This is a news item 2</div>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="https://source.unsplash.com/random"
-        title="Contemplative Reptile"
-        alt="News Item"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-      </>
+      <div className="card" style={{width: "18rem"}}>
+      <img src="https://assets1.cbsnewsstatic.com/hub/i/r/2022/08/07/13b85e18-cd47-4701-84bd-b221b140f3af/thumbnail/1200x630/a260f0ac37d6886125c719302cb69eeb/gettyimages-1242335423.jpg" className="card-img-top" alt="..."></img>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href="/" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    </>
     )
   }
-    
-  
 }
 
 export default NewsItem
