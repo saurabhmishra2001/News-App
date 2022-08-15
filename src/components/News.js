4c3f4e97acc31950608df3b99b2543f1f79f3534
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-08-15 13:34:49 +0530

    adding previous and next page for another pages

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import { ClosedCaptionDisabledSharp } from '@mui/icons-material';
+//import { ClosedCaptionDisabledSharp } from '@mui/icons-material';
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
@@ -9,28 +9,52 @@ export class News extends Component {
         console.log('News Component Constructor')
         this.state={
             articles:[],
-            loading:false
+            loading:false,
+            page:1
            
-           
-        }
+         }
     }
     async componentDidMount(){
         console.log('News Component Did Mount')
-        let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c39226d480ac4f7ebab08e04e6fbcabf';
+        let url = ' https://newsapi.org/v2/top-headlines?country=us&apiKey=c39226d480ac4f7ebab08e04e6fbcabf&page=1pageSize=20';
         
         let data=await fetch(url)
         let parsedData=await data.json()
             console.log(parsedData);
-            this.setState({articles:parsedData.articles})
+            this.setState({articles:parsedData.articles, totalResults:parsedData.totalResults})
     }
-    handlePrevClick=()=>{
+    handlePrevClick=async()=>{
         console.log('Prev Clicked')
-    }
-    handleNextClick=()=>{
-        console.log('Next Clicked');
+  
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c39226d480ac4f7ebab08e04e6fbcabf&page=${this.state.page-1}&pageSize=20`;
+        let data=await fetch(url)
+        let parsedData=await data.json()
+            console.log(parsedData);
+          
         this.setState({
+            page:this.state.page - 1,
+            articles:parsedData.articles
+            })
+    
+        }
+    handleNextClick=async()=>{
+        console.log('Next Clicked');
+        if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
+
+        }
+        else{
+
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c39226d480ac4f7ebab08e04e6fbcabf&page=${this.state.page+1}&pageSize=20`;
+        let data=await fetch(url)
+        let parsedData=await data.json()
+            console.log(parsedData);
+          
+        this.setState({
+            page:this.state.page + 1,
+            articles:parsedData.articles
             
-        })
+            })
+        }
     }
 
 
@@ -48,14 +72,12 @@ export class News extends Component {
                     } )}    
                 </div>
                 <div className="container d-flex justify-content-between">
-                    <button className="btn btn-sm btn-dark" onClick={this.handlePrevClick}>&larr;Previous</button>
+                    <button disabled={this.state.page<=1} className="btn btn-sm btn-dark" 
+                    onClick={this.handlePrevClick}>&larr;Previous</button>
                     <button className="btn btn-sm btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                     </div>
             </div>
         );
     } 
 }  
-        
- 
-
 export default News
